fd87d4e352307b7aa837e1d333951beb677feb6b
John <johnhoangn@gmail.com>
2019-03-21 21:33:03 -0400

    Added Home Fragment

---
+++ app/src/main/java/com/example/eatprosim/HomeFragment.kt
@@ -0,0 +1,21 @@
+package com.example.eatprosim
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val v = inflater.inflate(R.layout.fragment_home, container, false)
+
+        return v
+    }
+}
