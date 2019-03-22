5401fee0cfbb19afcbe6486040a7f5bf7254395e
John <johnhoangn@gmail.com>
2019-03-21 21:06:28 -0400

    test commit

--- app/src/main/java/com/example/eatprosim/FoodItem.kt
+++ app/src/main/java/com/example/eatprosim/FoodItem.kt
@@ -5,6 +5,5 @@ class FoodItem(price_arg : Double, name_arg : String, summary_arg : String) {
     val price = price_arg
     val name = name_arg
     val summary = summary_arg
-    val dummy = summary_arg
 
 }
